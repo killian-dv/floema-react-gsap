9449dc2a09cbdb3f6cd09d2e601203fc305f79de
killian-dv <killian.david@etudiant.univ-reims.fr>
2024-03-19 11:27:35 +0100

    init

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import Home from "@/pages/Home/Home.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles/index.scss";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
